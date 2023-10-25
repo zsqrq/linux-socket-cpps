b43c7e53f574e87b991f72dcd78214ee4825e88e
zsqrq <wz527193449@163.com>
2023-10-25 19:00:15 +0800

    add code

---
+++ unixip/lib/str_echo.cpp
@@ -0,0 +1,20 @@
+//
+// Created by wz on 23-10-25.
+//
+#include "common/unp.h"
+
+void str_echo(int sockfd) {
+  ssize_t n;
+  char buf[MAXLINE];
+  again:
+  while ((n = read(sockfd, buf, MAXLINE)) > 0) {
+    Writen(sockfd, buf, n);
+    if (n < 0 && errno == EINTR) {
+      goto again;
+    }
+    else if (n < 0) {
+      err_sys("str_echo : read error ");
+    }
+  }
+}
+
---
+++ unixip/tcpcliserv/sum.h
@@ -0,0 +1,12 @@
+//
+// Created by wz on 23-10-25.
+//
+struct args {
+  long	arg1;
+  long	arg2;
+};
+
+struct result {
+  long	sum;
+};
+
